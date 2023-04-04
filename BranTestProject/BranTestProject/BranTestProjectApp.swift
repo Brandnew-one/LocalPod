a9ba8a0fb4ced8d55a8ea4d4ac6daffc5aa4c42b
Bran <cold929@naver.com>
2023-04-04 15:01:16 +0900

    chore: project init

---
+++ BranTestProject/BranTestProject/BranTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranTestProjectApp.swift
+//  BranTestProject
+//
+//  Created by Bran on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct BranTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
