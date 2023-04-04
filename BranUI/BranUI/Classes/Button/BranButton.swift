5ed7545b87d325ef5f5db4751407ea32af66d674
Bran <cold929@naver.com>
2023-04-04 15:10:26 +0900

    fix: public button wrapper
    - BranUI를 사용하는 곳에서 사용할 수 있는 public view로 변경

--- BranUI/BranUI/Classes/Button/BranButton.swift
+++ BranUI/BranUI/Classes/Button/BranButton.swift
@@ -30,7 +30,7 @@ public enum ButtonColor {
   }
 }
 
-fileprivate struct CTAButtonWrapper: ViewModifier {
+fileprivate struct BranButtonWrapper: ViewModifier {
   private var buttonColor: ButtonColor
   let action: () -> Void
 
@@ -50,5 +50,35 @@ fileprivate struct CTAButtonWrapper: ViewModifier {
       .cornerRadius(16)
       .padding([.leading, .trailing], 1)
       .wrapToButton { action() }
+  }
+}
+
+extension View {
+  fileprivate func wrapToBranButton(
+    _ color: ButtonColor,
+    action: @escaping () -> Void
+  )  -> some View {
+    modifier(BranButtonWrapper(color, action: action))
+  }
+}
+
+public struct BranButton: View {
+  private var text: String
+  private var color: ButtonColor
+  let action: () -> Void
+
+  public init(
+    _ color: ButtonColor,
+    _ text: String,
+    action: @escaping () -> Void
+  ) {
+    self.color = color
+    self.text = text
+    self.action = action
+  }
+
+  public var body: some View {
+    Text(text)
+      .wrapToBranButton(color, action: action)
   }
 }
